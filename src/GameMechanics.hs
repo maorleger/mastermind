a88d32912a181a18912e3b935944a40042f09e4e
Maor Leger <maor.leger@gmail.com>
2016-09-30 14:51:33 -0600

    fixed repeated characters edge case and added tests

--- src/GameMechanics.hs
+++ src/GameMechanics.hs
@@ -1,8 +1,9 @@
 module GameMechanics where
 
-import Data.List (intersect)
+import Data.List (delete)
 import System.IO
 import System.Exit (exitSuccess)
+import Data.Char (toUpper)
 
 numRounds :: Int
 numRounds = 7
@@ -16,12 +17,16 @@ instance Monoid AnswerResult where
   mempty = AnswerResult 0 0
   mappend (AnswerResult x y) (AnswerResult x' y') = AnswerResult (x + x') (y + y')
 
-getIncorrectPositions :: Eq a => [(a,a)] -> Int
-getIncorrectPositions a = 
-  let 
-    answer = map fst a
-    guess = map snd a
-  in length $ intersect answer guess
+getIncorrectPositions :: Eq a => [a] -> [a] -> Int
+getIncorrectPositions _ [] = 0
+getIncorrectPositions [] _ = 0
+getIncorrectPositions answer (x':xs') = if x' `elem` answer then 1 + getIncorrectPositions (delete x' answer) xs' else 0 + getIncorrectPositions answer xs'
+  
+  -- let 
+  --   answer = map fst a
+  --   guess = map snd a
+  --   getIncorrectPositions' answer guess = 
+  -- in length $ intersect answer guess
 
 checkGuess :: Eq a => [a] -> [a] -> AnswerResult
 checkGuess answer guess = 
@@ -29,7 +34,7 @@ checkGuess answer guess =
     zippedGuesses = zip answer guess
     correctPositions = foldr (\(x, y) acc -> if x == y then acc + 1 else acc) 0 zippedGuesses
     leftOverPairs = filter (uncurry (/=)) zippedGuesses
-    incorrectPositions = getIncorrectPositions leftOverPairs
+    incorrectPositions = getIncorrectPositions (map fst leftOverPairs) (map snd leftOverPairs)
   in
     AnswerResult correctPositions incorrectPositions
 
@@ -40,16 +45,21 @@ endGame msg = do
   exitSuccess
 
 -- TODO: make this more generic, we should be able to handle many types
--- TODO: handle the case where our guess is of a different length than our answer....
 playRound :: String -> Int -> IO ()
 playRound answer roundNum = do
   putStr "Please enter a guess $ "
   hFlush stdout
   guess <- getLine
-  case checkGuess answer guess of
-    AnswerResult 4 0 -> endGame "You win!"
-    result -> do
-      putStr "Not quite... "
-      print result
-      if roundNum > numRounds then endGame "You lose" else playRound answer (roundNum + 1)
+  if length guess /= length answer then replayRound else checkRound guess
+  where 
+    replayRound = do
+      putStrLn $ "Your guess needs to be " ++ (show . length $ answer) ++ " characters long"
+      playRound answer roundNum
+    checkRound guess = 
+      case checkGuess answer (map toUpper guess) of
+        AnswerResult 4 0 -> endGame "You win!"
+        result -> do
+          putStr "Not quite... "
+          print result
+          if roundNum > numRounds then endGame "You lose" else playRound answer (roundNum + 1)
       
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,51 @@
+module Main where
+
+import Test.Hspec
+import Test.QuickCheck
+import Data.Monoid
+import GameMechanics
+
+instance Arbitrary AnswerResult where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return $ AnswerResult a b
+
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool 
+monoidAssoc a b c = (a `mappend` (b `mappend` c)) == ((a `mappend` b) `mappend` c)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (a `mappend` mempty) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (mempty `mappend` a) == a
+
+genMastermind :: Gen String
+genMastermind = do
+  a <- arbitrary `suchThat` (\x -> (length x) == 4)
+  return a
+
+prop_guessesCorrectly :: Property
+prop_guessesCorrectly =
+  forAll (genMastermind)
+  (\x -> checkGuess x x == AnswerResult 4 0)
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck (monoidAssoc :: AnswerResult -> AnswerResult -> AnswerResult -> Bool)
+  quickCheck (monoidRightIdentity :: AnswerResult -> Bool)
+  quickCheck (monoidLeftIdentity :: AnswerResult -> Bool)
+  quickCheck prop_guessesCorrectly
+
+
+  hspec $ do
+    describe "GameMechanics" $ do
+      it "Shows AnswerResult correctly" $ do
+        show (AnswerResult 4 0) `shouldBe` "[4,0]"
+      it "Correctly calculates incorrect positions" $ do
+        checkGuess "ABCD" "BCDA" `shouldBe` AnswerResult 0 4
+      it "Correctly handles repeated characters" $ do
+        checkGuess "AAAA" "AAAA" `shouldBe` AnswerResult 4 0
+        checkGuess "AABB" "ABAA" `shouldBe` AnswerResult 1 2
+        checkGuess "ABCF" "CBCA" `shouldBe` AnswerResult 2 1

eacc2090b03c743094eb15d8dfcde3ba9ca4f54e
Maor Leger <maor.leger@gmail.com>
2016-10-01 17:13:30 -0600

    using Either for validating the guess length

--- src/GameMechanics.hs
+++ src/GameMechanics.hs
@@ -26,40 +26,36 @@ instance Monoid AnswerResult where
 incorrectPositionsCalc :: Eq a => [a] -> [a] -> Int
 incorrectPositionsCalc _ [] = 0
 incorrectPositionsCalc [] _ = 0
-incorrectPositionsCalc answer (x':xs') = 
-  if x' `elem` answer 
+incorrectPositionsCalc answer (x':xs') =
+  if x' `elem` answer
     then 1 + incorrectPositionsCalc (delete x' answer) xs'
     else 0 + incorrectPositionsCalc answer xs'
 
 
-checkGuess :: Eq a => [a] -> [a] -> AnswerResult
-checkGuess answer guess = 
-  let 
-    zippedGuesses = zip answer guess
-    correctPositions = foldr (\(x, y) acc -> if x == y then acc + 1 else acc) 0 zippedGuesses
-    leftOverPairs = filter (uncurry (/=)) zippedGuesses
-    incorrectPositions = incorrectPositionsCalc (map fst leftOverPairs) (map snd leftOverPairs)
-  in
-    AnswerResult correctPositions incorrectPositions
-
-
-endGame :: String -> IO ()
-endGame msg = putStrLn msg >> exitSuccess
-
+checkGuess :: Eq a => [a] -> [a] -> Either String AnswerResult
+checkGuess answer guess
+  | length answer /= length guess =
+      Left $ "Your guess needs to be " ++ (show . length $ answer) ++ " characters long"
+  | otherwise =
+      let
+        zippedGuesses = zip answer guess
+        correctPositions = foldr (\(x, y) acc -> if x == y then acc + 1 else acc) 0 zippedGuesses
+        leftOverPairs = filter (uncurry (/=)) zippedGuesses
+        incorrectPositions = incorrectPositionsCalc (map fst leftOverPairs) (map snd leftOverPairs)
+      in
+        Right (AnswerResult correctPositions incorrectPositions)
 
 playRound :: String -> Int -> IO ()
-playRound answer roundNum = do
-  putStr "Please enter a guess $ "
-  hFlush stdout
-  guess <- getLine
-  if length guess /= length answer then replayRound else checkRound guess
-  where
-    replayRound = do
-      putStrLn $ "Your guess needs to be " ++ (show . length $ answer) ++ " characters long"
-      playRound answer roundNum
-    checkRound guess = 
-      case checkGuess answer (map toUpper guess) of
-        AnswerResult 4 0 -> endGame "You win!"
-        result -> putStrLn ("Not quite... " ++ show result) >>
-          if roundNum > numRounds then endGame ("Sorry, the correct code was: " ++ answer) else playRound answer (roundNum + 1)
-      
+playRound answer roundNum =
+  let endGame msg = putStrLn msg >> exitSuccess
+  in do
+    putStr "Please enter a guess $ "
+    hFlush stdout
+    guess <- getLine
+    case checkGuess answer (map toUpper guess) of
+      Left msg -> do
+        putStrLn msg
+        playRound answer roundNum
+      Right (AnswerResult 4 0) -> endGame "You win!"
+      Right result -> putStrLn ("Not quite... " ++ show result) >>
+        if roundNum > numRounds then endGame ("Sorry, the correct code was: " ++ answer) else playRound answer (roundNum + 1)

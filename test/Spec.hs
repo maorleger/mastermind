ef7a22e21167cd433ee7f3000de27614fcb7a3f2
Maor Leger <maor.leger@gmail.com>
2016-10-20 20:03:42 -0600

    delete tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -57,31 +57,3 @@ main = do
   quickCheck prop_scoreSumsCorrectly
   quickCheck prop_scoreSumsCorrectly'
 
-
-  hspec $ do
-    describe "GameMechanics" $ do
-      it "Shows AnswerResult correctly" $ do
-        show (AnswerResult 4 0) `shouldBe` "(4,0)"
-        show (AnswerResult 0 4) `shouldBe` "(0,4)"
-      it "Correctly calculates incorrect positions" $ do
-        checkGuess "ABCD" "BCDA" `shouldBe` AnswerResult 0 4
-      it "Correctly handles repeated characters" $ do
-        checkGuess "AAAA" "AAAA" `shouldBe` AnswerResult 4 0
-        checkGuess "AABB" "ABAA" `shouldBe` AnswerResult 1 2
-        checkGuess "ABCF" "CBCA" `shouldBe` AnswerResult 2 1
-      it "Correctly handles mismatched guess length" $ do
-        validateGuess "A" "AAAA" `shouldBe` Left "Your guess needs to be 1 characters long"
-        validateGuess "A" "AAAAA" `shouldBe` Left "Your guess needs to be 1 characters long"
-        validateGuess "AAAA" "A" `shouldBe` Left "Your guess needs to be 4 characters long"
-        validateGuess "AAAA" "AAAAA" `shouldBe` Left "Your guess needs to be 4 characters long"
-      it "Can find an inconsistent guess when one exists" $ do
-        inconsistent "AAAA" [CFG "AAAA" (AnswerResult 0 0)] `shouldBe` True
-        inconsistent "AAAA" [CFG "FFFF" (AnswerResult 3 1)] `shouldBe` True
-        inconsistent "AAAA" [] `shouldBe` False
-        inconsistent "AAAA" [CFG "BAAA" (AnswerResult 3 0)] `shouldBe` False
-      it "ReplaceAtIndex works" $ do
-        replaceAtIndex 0 'X' "ABCD" `shouldBe` "XBCD"
-        replaceAtIndex 0 'X' [] `shouldBe` []
-      it "CreateBlackCode works" $ do
-        createBlackCode "ABCD" [Nothing, Nothing, Nothing, Nothing] [0,2] `shouldBe` [Just 'A', Nothing, Just 'C', Nothing]
-        createBlackCode "ABCD" [Nothing, Nothing, Nothing, Nothing] [] `shouldBe` [Nothing, Nothing, Nothing, Nothing]

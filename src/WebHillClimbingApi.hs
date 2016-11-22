01b6a8309cb40629b9c0b6df96a7a3dd09d82112
Maor Leger <maor.leger@gmail.com>
2016-11-21 19:45:06 -0700

    got rid of some bad code

--- src/WebHillClimbingApi.hs
+++ src/WebHillClimbingApi.hs
@@ -30,34 +30,11 @@ genInitialCFG :: IO ApiCFG
 genInitialCFG = CodeBuilder.makeCode >>= (\code -> return $ ApiCFG code Nothing)
 
 
--- TODO: playRound needs to be replicated here
--- 1. Figure out how to end the game using the length of history
--- 2. figure out how to choose a CFG
--- 3. Generate a new guess
 playRound :: Game -> IO Game
 playRound (Game []) = do
   cfg <- genInitialCFG
   return $ Game [cfg]
 playRound (Game game@(cfg:history)) = do
   let numRounds = length (cfg:history)
-  -- assume that a (4,0) score wouldnt even make it here
-  -- because the elm frontend would declare win
   guess <- genCode (cfgConvert cfg) (fmap cfgConvert (cfg : history)) 0
   return . Game $ ApiCFG guess Nothing : game
-
-
-  -- playRound :: CFG -> Int -> [CFG] -> IO ()
-  -- playRound cfg@(CFG _ cfResult) roundNum history =
-  --   let
-  --     chooseCFG score guess =
-  --       if uncurry AnswerResult score > cfResult
-  --         then CFG guess (uncurry AnswerResult score)
-  --         else cfg
-  --   in do
-  --     guess <- genCode cfg history 0
-  --     putStrLn $ "Round: " <> show roundNum
-  --     putStrLn $ "My guess is: " <> show guess
-  --     putStrLn "How did I do?"
-  --     score <- readScore--' guess
-  --     checkForGameOver score roundNum
-  --     playRound (chooseCFG score guess) (roundNum + 1) (CFG guess (uncurry AnswerResult score) : history)
